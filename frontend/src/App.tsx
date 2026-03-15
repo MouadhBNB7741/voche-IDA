3f4d3787146d0aafe4b2d81b442b418468e75a27
Shivani Kapila <65486325+techsk81@users.noreply.github.com>
2026-03-15 23:23:45 +0000

    updating routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DataProvider } from "./contexts/DataContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { AuthProvider } from "./contexts/AuthContext";
+import { SettingsProvider } from "./contexts/SettingsContext";
 import './App.css'
 
 import Layout from "./components/Layout";
+import AuthLayout from "./components/AuthLayout";
 import Home from './pages/Home';
-import TrialSearch from './pages/Trials';
-import TrialDetail from "./pages/TrialDetail";
+import TrialSearch from './pages/Trials/Trials';
+import TrialDetail from "./pages/Trials/TrialDetail";
 import Community from './pages/Community/Community';
 import PostDetails from './pages/Community/PostDetails';
 import ResourceLibrary from './pages/Resources/ResourceLibrary';
@@ -14,34 +17,44 @@ import ResourceDetails from './pages/Resources/ResourceDetails';
 import EventsHub from './pages/Events/EventsHub';
 import EventDetails from './pages/Events/EventDetails';
 import Assistant from './pages/Assistant';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+
 
 export default function App() {
-
   return (
-
     <ThemeProvider>
-      <DataProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/trials" element={<TrialSearch />} />
-              <Route path="/trials/:id" element={<TrialDetail />} />
-              <Route path="/community" element={<Community />} />
-              <Route path="/community/posts/:id" element={<PostDetails />} />
-              <Route path="/resourcelibrary" element={<ResourceLibrary />} />
-              <Route path="/resources/:id" element={<ResourceDetails />} />
-              <Route path="/eventshub" element={<EventsHub />} />
-              <Route path="/events/:id" element={<EventDetails />} />
-              <Route path="/assistant" element={<Assistant />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </DataProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <DataProvider>
+            <BrowserRouter>
+              <Routes>
+
+                <Route element={<AuthLayout />}>
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                </Route>
+
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/trials" element={<TrialSearch />} />
+                  <Route path="/trials/:id" element={<TrialDetail />} />
+                  <Route path="/community" element={<Community />} />
+                  <Route path="/community/posts/:id" element={<PostDetails />} />
+                  <Route path="/resourcelibrary" element={<ResourceLibrary />} />
+                  <Route path="/resources/:id" element={<ResourceDetails />} />
+                  <Route path="/eventshub" element={<EventsHub />} />
+                  <Route path="/events/:id" element={<EventDetails />} />
+                  <Route path="/assistant" element={<Assistant />} />
+                  <Route path="/profile" element={<Profile />} />
+                </Route>
+
+              </Routes>
+            </BrowserRouter>
+          </DataProvider>
+        </SettingsProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
